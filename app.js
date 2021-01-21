7a95ffc0f63b96c9e1806ad95d916dcc8a34f39f
Serverguyken <serverguyken@gmail.com>
2021-01-21 04:39:50 -0500

    using express sending message back to client

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+const express = require('express');
+const app = express();
+const port = 3000 || process.env.PORT
 const fetch = require('node-fetch');
 const cron = require('node-cron')
 require('dotenv').config();
@@ -8,22 +11,31 @@ const authToken = process.env.TWILIO_AUTH_TOKEN;
 const client = require('twilio')(accountSid,authToken);
 
 
-async function getQuotes(url) {
-    let quotes = [];
-    let response = fetch(url);
-    let data = (await response).json();
-    quotes = data;
-    let quoteNumber;
-    quotes.then(quotes => {
-        quoteNumber = (Math.random() * 1000).toFixed();
-            const quoteMessage = `Hey Kenny, here's a quote to start your day 😀. \n**--${quotes[quoteNumber].text}--** @${quotes[quoteNumber].author ? quotes[quoteNumber].author : "No author" ? quotes[quoteNumber].author === "undefined" : 'No author'}`;
-            console.log(quoteMessage);
-            client.messages.create({
-                to: process.env.TO_NUMBER,
-                from: process.env.TWILIO_ACCOUNT_NUMBER,
-                body: quoteMessage
-            }).then(message => console.log(message.sid));
-    })
-}
 
-getQuotes('https://type.fit/api/quotes');
+app.get('/', (req,res) => {
+    res.json({
+        "message": "Sending inspirational quotes now"
+    });
+    async function getQuotes(url) {
+        let quotes = [];
+        let response = fetch(url);
+        let data = (await response).json();
+        quotes = data;
+        let quoteNumber;
+        quotes.then(quotes => {
+            quoteNumber = (Math.random() * 1000).toFixed();
+                const quoteMessage = `Hey Kenny, here's a quote to start your day 😀. \n**--${quotes[quoteNumber].text}--** @${quotes[quoteNumber].author ? quotes[quoteNumber].author : "No author" ? quotes[quoteNumber].author === "undefined" : 'No author'}`;
+                console.log(quoteMessage);
+                client.messages.create({
+                    to: process.env.TO_NUMBER,
+                    from: process.env.TWILIO_ACCOUNT_NUMBER,
+                    body: quoteMessage
+                }).then(message => console.log(message.sid));
+        })
+    }
+    
+    getQuotes('https://type.fit/api/quotes');
+})
+
+app.listen(port, console.log(`Server started on ${port}`))
+
