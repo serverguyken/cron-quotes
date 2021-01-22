5847115fb6f42ecc42d52a5075d3875d05d8bbef
Serverguyken <serverguyken@gmail.com>
2021-01-22 01:08:23 -0500

    twilio sms username required

--- app.js
+++ app.js
@@ -12,28 +12,30 @@ const client = require('twilio')(accountSid,authToken);
 
 
 
+async function getQuotes(url) {
+    let quotes = [];
+    let response = fetch(url);
+    let data = (await response).json();
+    quotes = data;
+    let quoteNumber;
+    quotes.then(quotes => {
+        quoteNumber = (Math.random() * 1000).toFixed();
+            const quoteMessage = `Hey Kenny, here's a quote to start your day 😀. \n**--${quotes[quoteNumber].text}--** @${quotes[quoteNumber].author ? quotes[quoteNumber].author : "No author" ? quotes[quoteNumber].author === "undefined" : 'No author'}`;
+            console.log(quoteMessage);
+            client.messages.create({
+                to: process.env.TO_NUMBER,
+                from: process.env.TWILIO_ACCOUNT_NUMBER,
+                body: quoteMessage
+            }).then(message => console.log(`Message sid => ${message.sid}`)).catch(error => console.log(error))
+    })
+}
+getQuotes('https://type.fit/api/quotes');
+
 app.get('/', (req,res) => {
     res.json({
         "message": "Sending inspirational quotes now"
     });
-    async function getQuotes(url) {
-        let quotes = [];
-        let response = fetch(url);
-        let data = (await response).json();
-        quotes = data;
-        let quoteNumber;
-        quotes.then(quotes => {
-            quoteNumber = (Math.random() * 1000).toFixed();
-                const quoteMessage = `Hey Kenny, here's a quote to start your day 😀. \n**--${quotes[quoteNumber].text}--** @${quotes[quoteNumber].author ? quotes[quoteNumber].author : "No author" ? quotes[quoteNumber].author === "undefined" : 'No author'}`;
-                console.log(quoteMessage);
-                client.messages.create({
-                    to: process.env.TO_NUMBER,
-                    from: process.env.TWILIO_ACCOUNT_NUMBER,
-                    body: quoteMessage
-                }).then(message => console.log(`Message sid => ${message.sid}`)).catch(error => console.log(error))
-        })
-    }
-    getQuotes('https://type.fit/api/quotes');
+    
 })
 
 app.listen(port, console.log(`Server started on ${port}`))
